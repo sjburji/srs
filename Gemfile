fc0601a077d91f82979e0feb9a5b6d09c2281d51
Supreeth Burji <supreethjburji@yahoo.com>
2011-12-31 04:42:49 +0530

    deployment updates

--- Gemfile
+++ Gemfile
@@ -9,17 +9,13 @@ gem 'sunspot_rails', '1.2.rc4'
 gem "will_paginate", "~> 3.0.pre2"
 gem "transitions", :require => ["transitions", "active_record/transitions"]
 gem 'jquery-rails'
-#gem 'activerecord-oracle_enhanced-adapter'
 gem 'acts_as_versioned'
-gem 'mysql'
-gem 'rake', "~> 0.9.2.2"
-#gem 'ruby-oci8'
+gem 'rake'
+gem 'capistrano'
 
 # Use unicorn as the web server
 # gem 'unicorn'
 
-# Deploy with Capistrano
-# gem 'capistrano'
 
 # To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
 # gem 'ruby-debug'
---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+set :user, 'srspal'  # Your dreamhost account's username
+set :domain, 'pulcherrima.dreamhost.com'  # Dreamhost servername where your account is located 
+set :project, 'srs'  # Your application as its called in the repository
+set :application, 'srspal.com'  # Your app's location (domain or sub-domain name as setup in panel)
+set :applicationdir, "/home/#{user}/#{application}"  # The standard Dreamhost setup
+set :deploy_via, :remote_cache
+set :scm, :git
+
+# version control config
+set :scm_username, 'sjburji'
+set :repository,  "git@github.com:sjburji/srs.git"
+
+# roles (servers)
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
+
+# deploy config
+set :deploy_to, applicationdir
+set :deploy_via, :export
+
+# additional settings
+default_run_options[:pty] = true  # Forgo errors when deploying from windows
+ssh_options[:keys] = %w(/Path/To/id_rsa)            # If you are using ssh_keys
+set :chmod755, "app config db lib public vendor script script/* public/disp*"
+set :use_sudo, false
