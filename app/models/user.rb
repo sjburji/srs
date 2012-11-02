19c6a211350902222b51fc812ecf15bdece1bad6
Supreeth Burji <supreethjburji@yahoo.com>
2012-11-02 18:24:23 +0530

    Secure random update for legacy ruby support

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ActiveRecord::Base
 
   def generate_token(column)
     begin
-      self[column] = SecureRandom.urlsafe_base64
+      self[column] = SecureRandom.hex
     end while User.exists?(column => self[column])
   end
 
