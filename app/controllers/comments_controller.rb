fbf4351fbd0765fff89b94a89488c8ba0c834397
Supreeth Burji <supreethjburji@yahoo.com>
2012-11-10 12:09:31 +0530

    adding comments enhancements

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,33 +24,48 @@ class CommentsController < ApplicationController
   # GET /comments/new
   # GET /comments/new.xml
   def new
-    @comment = Comment.new
-    @comment.post_id = params[:post_id].to_i
-    @comment.user_id = current_user.id
+    if signed_in?
+      @comment = Comment.new
+      @comment.post_id = params[:post_id].to_i
+      @comment.user_id = current_user.id
 
-    unless params[:parent_id].nil?
-      @comment.parent_id = params[:parent_id].to_i
+      unless params[:parent_id].nil?
+        @comment.parent_id = params[:parent_id].to_i
+      end
+
+      render :partial => 'form'
+    else
+      redirect_to(root_path, :alert => "Invalid user, Please sign in and try again")
     end
-
-    render :partial => 'form'
   end
 
   # GET /comments/1/edit
   def edit
     @comment = Comment.find(params[:id])
+    if signed_in? && @comment.user_id.to_i == current_user.id
+      respond_to do |format|
+        format.html
+      end
+    else
+      redirect_to(root_path, :alert => "Invalid user, Please sign in and try again")
+    end
   end
 
   # POST /comments
   # POST /comments.xml
   def create
-    @comment = Comment.new(params[:comment])
+    if signed_in? && params[:comment][:user_id].to_i == current_user.id
+      @comment = Comment.new(params[:comment])
 
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to(root_path) }
-      else
-        format.html { render :partial => 'form' }
+      respond_to do |format|
+        if @comment.save
+          format.html { redirect_to(root_path) }
+        else
+          format.html { render :partial => 'form' }
+        end
       end
+    else
+      redirect_to(root_path, :alert => "Invalid user, Please sign in and try again")
     end
   end
 
@@ -58,15 +73,18 @@ class CommentsController < ApplicationController
   # PUT /comments/1.xml
   def update
     @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      if @comment.update_attributes(params[:comment])
-        format.html { redirect_to(@comment, :notice => 'Comment was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
+    if signed_in? && @comment.user_id.to_i == current_user.id
+      respond_to do |format|
+        if @comment.update_attributes(params[:comment])
+          format.html { redirect_to(@comment, :notice => 'Comment was successfully updated.') }
+          format.xml  { head :ok }
+        else
+          format.html { render :action => "edit" }
+          format.xml  { render :xml => @comment.errors, :status => :unprocessable_entity }
+        end
       end
+    else
+      redirect_to(root_path, :alert => "Invalid user, Please sign in and try again")
     end
   end
 
@@ -74,11 +92,14 @@ class CommentsController < ApplicationController
   # DELETE /comments/1.xml
   def destroy
     @comment = Comment.find(params[:id])
-    @comment.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(comments_url) }
-      format.xml  { head :ok }
+    if signed_in? && author_signed_in?
+      @comment.destroy
+      respond_to do |format|
+        format.html { redirect_to(comments_url) }
+        format.xml  { head :ok }
+      end
+    else
+      redirect_to(root_path, :alert => "Invalid user, Please sign in and try again")
     end
   end
 
